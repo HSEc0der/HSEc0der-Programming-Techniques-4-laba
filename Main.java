32ad24cc30265179934f3432251bd6cd894265d0
HSEc0der <55021869+HSEc0der@users.noreply.github.com>
2020-05-28 02:33:59 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+import static laba_4.Generators.*;
+import static laba_4.Measure.*;
+
+import java.math.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        for (int i = 0; i < 1; i++) {
+            help(1291, 4621, 21870, 10000, 0);
+        }
+        int[] arr = {1000, 10_000, 50_000, 100_000, 200_000, 400_000, 700_000, 1_000_000, 1_500_000, 2_000_000};
+        //timeCheck(arr);
+        //writeToFile(1291,4621,21870,10000,0);
+    }
+}
